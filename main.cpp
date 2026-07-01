33263b076027dac424521491d9c9f686f98b2085
6876h9 <burairabbas915@gmail.com>
2026-07-01 15:11:39 +0500

    Clear repository

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include "driver.h"
-
-int main(int argc, char** argv) {
-    cminus::CompilerOptions opts;
-    if (!cminus::parse_args(argc, argv, opts)) {
-        return 1;
-    }
-    return cminus::compile(opts);
-}
